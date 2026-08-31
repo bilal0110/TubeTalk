074ff8ead678457e89934b7b0b3a32c03c341d14
bilal0110 <bilalmohd175@gmail.com>
2026-08-31 11:59:04 +0530

    Fix failing CI test

--- tests/test_gradio_app.py
+++ tests/test_gradio_app.py
@@ -8,7 +8,7 @@ from gradio_app import extract_video_id
 
 def test_extract_video_id_from_watch_url():
     url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-    assert extract_video_id(url) == "WRONG ID"
+    assert extract_video_id(url) == "dQw4w9WgXcQ"
 
 
 def test_extract_video_id_from_short_url():
